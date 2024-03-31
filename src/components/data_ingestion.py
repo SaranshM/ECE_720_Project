08a508548cc4cd52655086260e2fc6594909933a
SaranshM <saransh.canada.2022@gmail.com>
2024-03-30 18:52:18 -0600

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
+# temp
+from src.components.data_transformation import DataTransformation
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifacts", "train.csv")
@@ -23,9 +27,41 @@ class DataIngestion:
             df = pd.read_csv('src/notebook/data/smoking.csv')
             logging.info("Read the dataset")
 
+            df = df.drop(['ID'], axis = 1)
+
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
 
             df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
+
+            logging.info("Renaming columns")
+
+            new_column_names = {
+                "age": "age",
+                "height(cm)": "height",
+                "weight(kg)": "weight",
+                "waist(cm)": "waist",
+                "eyesight(left)": "eyesight_left",
+                "eyesight(right)": "eyesight_right",
+                "hearing(left)": "hearing_left",
+                "hearing(right)": "hearing_right",
+                "systolic": "systolic",
+                "relaxation": "relaxation",
+                "fasting blood sugar": "fasting_blood_sugar",
+                "Cholesterol": "cholesterol",
+                "triglyceride": "triglyceride",
+                "HDL": "hdl",
+                "LDL": "ldl",
+                "hemoglobin": "hemoglobin",
+                "Urine protein": "urine_protein",
+                "serum creatinine": "serum_creatinine",
+                "AST": "ast",
+                "ALT": "alt",
+                "Gtp": "gtp",
+                "dental caries": "dental_caries",
+                "smoking": "smoker_status"
+            }
+
+            df.rename(columns = new_column_names, inplace = True)
 
             logging.info("Initiating train-test split")
             train_set, test_set = train_test_split(df, random_state = 42, test_size = 0.15)
@@ -44,6 +80,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     data_ingestion_obj = DataIngestion()
-    data_ingestion_obj.initiate_data_ingestion()
+    train_data, test_data = data_ingestion_obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, preprocessor_file_path = data_transformation.initiate_data_transformation(train_data, test_data)
 
 
